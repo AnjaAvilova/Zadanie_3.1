ebbdadd4cfdb10b72049abc7c58c68847a497d81
Anna A <markova29@yandex.ru>
2023-02-26 21:28:22 +0300

    gitignore all

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus;
+        int ticket = 21000;
+        int amount = 20;
+
+        System.out.println(bonus = ticket / amount);
+
+    }
+}
